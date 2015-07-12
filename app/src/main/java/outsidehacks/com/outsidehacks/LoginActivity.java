5903b88f46513a5e15f1a985348cf6599b911afc
Neel Kothari <neel@Neels-MacBook-Air.local>
2015-07-12 01:48:45 -0700

    reached first major check point

--- app/src/main/java/outsidehacks/com/outsidehacks/LoginActivity.java
+++ app/src/main/java/outsidehacks/com/outsidehacks/LoginActivity.java
@@ -1,25 +1,186 @@
 package outsidehacks.com.outsidehacks;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Config;
 import com.spotify.sdk.android.player.Spotify;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerNotificationCallback;
+import com.spotify.sdk.android.player.PlayerState;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyCallback;
+import kaaes.spotify.webapi.android.SpotifyError;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Album;
+import kaaes.spotify.webapi.android.models.Pager;
+import kaaes.spotify.webapi.android.models.Playlist;
+import kaaes.spotify.webapi.android.models.PlaylistSimple;
+import kaaes.spotify.webapi.android.models.UserPrivate;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends Activity implements PlayerNotificationCallback, ConnectionStateCallback {
+
+    private static final String CLIENT_ID = "555de185a8e241bd95f34f7dd43f3d4c";
+    private static final String REDIRECT_URI = "outsidehacks://callback";
+
+    private Button spotifyLoginBtn;
+
+    // Request code that will be used to verify if the result comes from correct activity
+    // Can be any integer
+    private static final int REQUEST_CODE = 1337;
+    private static final String PLAYLIST_DEFAULT = "OutsideHacks";
+    private static String ACCESS_TOKEN;
+    private static String USER_ID;
+    private static String PLAYLIST_ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        AuthenticationRequest.Builder builder
+                = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "playlist-modify-public", "playlist-modify-private", "playlist-read-private"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
 
-        Intent intent = new Intent(this, RecordActivity.class);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
+                Log.d("LoginActivity", response.getAccessToken());
+                ACCESS_TOKEN = response.getAccessToken();
+                fetchOrCreateDefaultPlaylist(response.getAccessToken());
+            }
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("LoginActivity", "User logged in");
+    }
+
+    @Override
+    public void onLoggedOut() {
+        Log.d("LoginActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Throwable error) {
+        Log.d("LoginActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("LoginActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("LoginActivity", "Received connection message: " + message);
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+        Log.d("LoginActivity", "Playback event received: " + eventType.name());
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String errorDetails) {
+        Log.d("LoginActivity", "Playback error received: " + errorType.name());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void fetchOrCreateDefaultPlaylist(String accessToken) {
+        SpotifyApi api = new SpotifyApi();
+        api.setAccessToken(accessToken);
+        final SpotifyService spotify = api.getService();
+        spotify.getMe(new SpotifyCallback<UserPrivate>() {
+            @Override
+            public void failure(SpotifyError spotifyError) {
+                Log.v("LoginActivity", "Error: " + spotifyError);
+            }
+
+            @Override
+            public void success(final UserPrivate userPrivate, Response response) {
+                Log.v("LoginActivity","Retrieved User Id: " + userPrivate.id);
+                USER_ID = userPrivate.id;
+                spotify.getPlaylists(userPrivate.id, new SpotifyCallback<Pager<PlaylistSimple>>() {
+                    @Override
+                    public void failure(SpotifyError spotifyError) {
+                        Log.v("LoginActivity", "Error: " + spotifyError);
+                    }
+
+                    @Override
+                    public void success(Pager<PlaylistSimple> playlistSimplePager, Response response) {
+                        List<PlaylistSimple> playlists = playlistSimplePager.items;
+                        for (PlaylistSimple playlist:playlists) {
+                            Log.v("LoginActivity", "Retrieved Playlist: "+ playlist.name);
+                            if (playlist.name.equals(PLAYLIST_DEFAULT)) {
+                                Log.v("LoginActivity", "Default Playlist EXISTS");
+                                PLAYLIST_ID = playlist.id;
+                                moveToTrackActivity();
+                                return;
+                            }
+                        }
+                        Log.v("LoginActivity", "Default Playlist DOES NOT EXIST");
+                        Map<String, Object> body = new HashMap<String, Object>();
+                        body.put("name", PLAYLIST_DEFAULT);
+                        body.put("public", false);
+                        spotify.createPlaylist(userPrivate.id, body, new SpotifyCallback<Playlist>() {
+                            @Override
+                            public void failure(SpotifyError spotifyError) {
+                                Log.v("LoginActivity", "Error: " + spotifyError);
+                            }
+
+                            @Override
+                            public void success(Playlist playlist, Response response) {
+                                Log.v("LoginActivity", "Created Default Playlist");
+                                PLAYLIST_ID = playlist.id;
+                                moveToTrackActivity();
+                                return;
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+    }
+
+    public void moveToTrackActivity() {
+        Intent intent = new Intent(this, TrackActivity.class);
+        intent.putExtra("ACCESS_TOKEN", ACCESS_TOKEN);
+        intent.putExtra("USER_ID", USER_ID);
+        intent.putExtra("PLAYLIST_ID", PLAYLIST_ID);
         startActivity(intent);
-
+        finish();
     }
 }
--- app/src/main/java/outsidehacks/com/outsidehacks/TrackActivity.java
+++ app/src/main/java/outsidehacks/com/outsidehacks/TrackActivity.java
@@ -2,16 +2,134 @@ package outsidehacks.com.outsidehacks;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyCallback;
+import kaaes.spotify.webapi.android.SpotifyError;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistSimple;
+import kaaes.spotify.webapi.android.models.Pager;
+import kaaes.spotify.webapi.android.models.PlaylistTrack;
+import kaaes.spotify.webapi.android.models.Track;
+import kaaes.spotify.webapi.android.models.TracksPager;
+import retrofit.client.Response;
 
 
 public class TrackActivity extends ActionBarActivity {
+
+    private static String ACCESS_TOKEN;
+    private static String USER_ID;
+    private static String PLAYLIST_ID;
+    private String artistQuery;
+    private String trackQuery;
+
+    private EditText artistET;
+    private EditText trackET;
+    private ImageView trackAlbumIV;
+    private Button saveTrackToSpotifyBtn;
+    private TextView artistTV;
+    private TextView trackTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track);
+        artistET = (EditText)findViewById(R.id.et_artist);
+        trackET = (EditText)findViewById(R.id.et_track);
+        saveTrackToSpotifyBtn = (Button)findViewById(R.id.btn_save_track);
+        trackAlbumIV = (ImageView)findViewById(R.id.iv_track);
+        trackTV = (TextView)findViewById(R.id.tv_title);
+        artistTV = (TextView)findViewById(R.id.tv_artist);
+
+        ACCESS_TOKEN = getIntent().getStringExtra("ACCESS_TOKEN");
+        USER_ID = getIntent().getStringExtra("USER_ID");
+        PLAYLIST_ID = getIntent().getStringExtra("PLAYLIST_ID");
+
+        SpotifyApi api = new SpotifyApi();
+        api.setAccessToken(ACCESS_TOKEN);
+        final SpotifyService spotify = api.getService();
+
+        saveTrackToSpotifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                artistQuery = artistET.getText().toString();
+                trackQuery = trackET.getText().toString();
+                String query = trackQuery.replace(" ", "+") + '+' + artistQuery.replace(" ", "+");
+                Log.v("TrackActivity", query);
+//                Map<String, Object> options = new HashMap<String, Object>();
+//                options.put()
+//                spotify.searchTracks(query, options, new SpotifyCallback<TracksPager>() {
+//                    @Override
+//                    public void failure(SpotifyError spotifyError) {
+//
+//                    }
+//
+//                    @Override
+//                    public void success(TracksPager tracksPager, Response response) {
+//
+//                    }
+//                });
+                spotify.searchTracks(trackQuery, new SpotifyCallback<TracksPager>() {
+                    @Override
+                    public void failure(SpotifyError spotifyError) {
+                        Log.v("TrackActivity", "Error: " + spotifyError);
+                    }
+
+                    @Override
+                    public void success(TracksPager tracksPager, Response response) {
+                        List<Track> tracks = tracksPager.tracks.items;
+                        for (Track track: tracks) {
+//                            Log.v("TrackActivity", track.name);
+                            List<ArtistSimple> artists = track.artists;
+                            for (ArtistSimple artist: artists) {
+                                if(artist.name.toLowerCase().replace(" ", "").equals(artistQuery.toLowerCase().replace(" ", ""))){
+                                    Log.v("TrackActivity", "Track Selected: " + track.name);
+                                    trackTV.setText(track.name);
+                                    artistTV.setText(artist.name);
+                                    Picasso.with(getApplicationContext()).load(track.album.images.get(0).url).into(trackAlbumIV);
+                                    Map<String, Object> parameters = new HashMap<String, Object>();
+                                    parameters.put("uris", track.uri);
+                                    spotify.addTracksToPlaylist(USER_ID, PLAYLIST_ID, parameters, parameters, new SpotifyCallback<Pager<PlaylistTrack>>() {
+                                        @Override
+                                        public void failure(SpotifyError spotifyError) {
+                                            Log.v("TrackActivity", "Error: " + spotifyError);
+                                        }
+
+                                        @Override
+                                        public void success(Pager<PlaylistTrack> playlistTrackPager, Response response) {
+                                            Log.v("TrackActivity", "Successfully added to playlist");
+                                        }
+                                    });
+                                    return;
+                                } else {
+                                    Log.v("TrackActivity",artist.name.toLowerCase().replace(" ", "") + " to " + artistQuery.toLowerCase().replace(" ", ""));
+                                    Log.v("TrackActivity", artist.name + " : " + track.name);
+                                }
+                            }
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+
     }
 
 }
