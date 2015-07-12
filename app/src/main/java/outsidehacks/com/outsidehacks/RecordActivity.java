9540e8f6751bd01b15876dcc8a718c59de0e71f3
sourabhdesai <sourabhdesai@gmail.com>
2015-07-11 17:43:59 -0700

    init commit

---
+++ app/src/main/java/outsidehacks/com/outsidehacks/RecordActivity.java
@@ -0,0 +1,16 @@
+package outsidehacks.com.outsidehacks;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class RecordActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+    }
+}
